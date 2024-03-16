a067accdd3394ba5583a3b85f299c75c69d0f894
AudreyHuang0206 <sweetin0226@gmail.com>
2024-03-16 17:15:49 +0800

    if, elif, else practice

---
+++ #homework10.py
@@ -0,0 +1,20 @@
+#homework10
+'''Given an integer score between 0 and 100 (inclusive), 
+determine the student’s grade based on the following criteria:
+If score >= 90, the grade is “A”.
+If score >= 80, the grade is “B”.
+If score >= 70, the grade is “C”.
+If score >= 60, the grade is “D”.
+Otherwise, the grade is “F”.
+For example, if score = 85, the output should be “B”.'''
+score = int(input ('please enter your score :'))
+if score >= 90 :
+    print('the grade is "A"' )
+elif score >= 80 :
+    print('the grade is "A"' )
+elif score >= 70 :
+    print('the grade is "A"' )
+elif score >= 60 :
+    print('the grade is "A"' )
+else :
+    print('the grade is "F"' )
