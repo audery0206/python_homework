8e0af020d6f10d8138a72d3d0098d2e43e6b088a
AudreyHuang0206 <sweetin0226@gmail.com>
2024-05-12 15:59:21 +0800

    recursive, and iterative method practice
    Fibonacci sequence

--- #homework14.py
+++ #homework14.py
@@ -1,7 +1,26 @@
 #homework14
 '''Question 1: Recursive Calculation of Fibonacci Sequence
 Difficulty: Easy
-Description: The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, typically starting with 0 and 1. Given a positive integer n, please calculate the nth term of the Fibonacci sequence using recursion.
+Description: The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones, typically starting with 0 and 1. 
+Given a positive integer n, please calculate the nth term of the Fibonacci sequence using recursion.
 Example:
 Input: n = 5
 Output: 5'''
+
+#費氏數列 using recursion,用時間換取空間（記憶體）
+#x=0,1,1,2,3,5,8,13,21....
+'''
+n=input()
+for i in range(3,n+1)
+    i=0, f(0)=0
+    i=1, f(1)=1
+    f(i)=f(i-1)+f(i-2)
+'''
+x=int(input("enter a number for verify Fibonacci sequence:"))
+def fib(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fib(n-1)+fib(n-2)
+print(fib(x))
--- #homework15.py
+++ #homework15.py
@@ -1,7 +1,19 @@
 #homework15
 '''Question 2: Dynamic Programming Calculation of Fibonacci Sequence
 Difficulty: Medium
-Description: Same as the previous question, we are dealing with the Fibonacci sequence. This time, please calculate the nth term of the Fibonacci sequence using dynamic programming and optimize the space complexity to O(1).
+Description: Same as the previous question, we are dealing with the Fibonacci sequence. 
+This time, please calculate the nth term of the Fibonacci sequence using dynamic programming and optimize the space complexity to O(1).
 Example:
 Input: n = 20
 Output: 6765'''
+#費氏數列 using iterative(dynamic program)用記憶體換時間
+n=int(input("enter a number for verify Fibonacci sequence:"))
+f={
+    1:1,
+    2:1,
+}
+for i in range(3, n+1):
+    f[i]=f[i-1]+f[i-2]
+#print(f.value(n))
+#print(f"The Fibonacci value of {n} is: {f[n]}") -> 為何大掛號會印出來？
+print(f[n])
