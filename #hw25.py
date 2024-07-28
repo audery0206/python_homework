bc3da97545c377a57302e046cba6487ca0bff43f
AudreyHuang0206 <sweetin0226@gmail.com>
2024-07-28 16:42:30 +0800

    Create #hw25.py

---
+++ #hw25.py
@@ -0,0 +1,8 @@
+#hw25
+'''Please enter the number of rows you want to display, 
+and each row will show a corresponding number of *'''
+
+row = int(input('enter how many row you want to show :'))
+for i in range(1, row+1):
+    print('*'*i)
+    
